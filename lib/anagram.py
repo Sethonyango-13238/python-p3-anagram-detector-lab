4232f30a9fa3428932e79293e0231f37822f669d
Sethonyango-13238 <onyangosethoyugi@gmail.com>
2025-08-09 08:44:01 +0300

    Anagram lab completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+    def match(self,words):
+            sorted_word=sorted(self.word)
+            matches=[]
+
+            for candidate in words:
+                if candidate.lower() ==self.word:
+                    continue
+                if sorted(candidate.lower())==sorted_word:
+                    matches.append(candidate)
+            return matches
+
